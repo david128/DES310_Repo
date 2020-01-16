af8295500a6b3ad37a224f1f0fd2261ceb5f32ac
d <d>
2020-01-16 22:55:05 +0000

    Started on mouse input
    Co-Authored-By: Lee <leegillan@users.noreply.github.com>

---
+++ DES310_Game/Assets/Script/AssetChange.cs
@@ -0,0 +1,45 @@
+﻿/*
+Asset change function
+
+Lee Gillan, David Ireland
+16/01/2020 
+*/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetChange : MonoBehaviour
+{
+    bool whichShape; //true for sphere, false for cube
+
+
+    void start()
+    {
+        
+    }
+
+    //check for input
+    void GetInput()
+    {
+        Debug.Log("input");
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("hit");
+            }
+        }
+    }
+
+    //everything to be updated
+    void update()
+    {
+        //get input
+        GetInput();
+    }
+
+}
