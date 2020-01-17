6a4f1264a04a03a58070f7c41666269d504c4e7c
Lee <170170@uad.ac.uk>
2020-01-17 17:20:20 +0000

    Change Shape Mechanic
    Added ability to change shape through mouse click. Cube -> Sphere and back.
    
    Co-Authored-By: david128 <david128@users.noreply.github.com>

--- DES310_Game/Assets/Script/AssetChange.cs
+++ DES310_Game/Assets/Script/AssetChange.cs
@@ -14,29 +14,62 @@ public class AssetChange : MonoBehaviour
 {
     bool whichShape; //true for sphere, false for cube
 
-
-    void start()
+    void Start()
     {
-        
+ 
+    }
+
+    void ChangeAsset()
+    {
+        //Declare variables
+        GameObject shape;
+        Transform transform;
+
+        //find shape objects
+        shape = GameObject.FindGameObjectWithTag("Shape");
+
+        //get shape transform
+        transform = shape.transform;
+
+        //Destroy shape to be replaced
+        GameObject.Destroy(shape);
+
+        //check what the shape is currently at adn instantiate the other shape
+        if (whichShape == false)
+        {
+            GameObject.Instantiate(Resources.Load("Sphere"), transform.position, transform.rotation);
+            whichShape = true;
+        }
+        else
+        {
+            GameObject.Instantiate(Resources.Load("Cube"), transform.position, transform.rotation);
+            whichShape = false;
+        }
     }
 
     //check for input
     void GetInput()
     {
-        Debug.Log("input");
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
+
+            //Check what has been clicked/touched//
+
             if (Physics.Raycast(ray, out hit))
             {
                 Debug.Log("hit");
+
+                //Call change function//
+                ChangeAsset();
+
             }
         }
     }
 
     //everything to be updated
-    void update()
+    void Update()
     {
         //get input
         GetInput();
