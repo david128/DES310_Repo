7835a7f2c1a3a313af6962733f652a50ad24b8d3
david128 <davidireland128@gmail.com>
2020-01-21 10:57:53 +0000

    Input Script
    Started writing input script

--- DES310_Game/Assets/Script/InputScript.cs
+++ DES310_Game/Assets/Script/InputScript.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class InputScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public bool controlType;//true for mobile, false for pc
+
+    //get Inut to be called in main game loop
+    void GetInput()
     {
-        
+        if (controlType == false)//pc
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hit;
+
+                //casts a ray from camera to mouse position
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                //Check what has been clicked/touched//
+
+
+                //Checks if the ray connects with an object/asset
+                if (Physics.Raycast(ray, out hit))
+                {
+                    int targetType = hit.collider.gameObject.GetComponent<ObjectInfo>().GetObjectType(); //object we are clicking
+                    switch(targetType)
+                    {
+                        case 1:
+                            //run selet object type 1 select
+                            break;
+
+                        default:
+                            //run default click action
+                            break;
+                    }
+                    
+
+                }
+                }
+                }
+            }
+
+        }
     }
-}
