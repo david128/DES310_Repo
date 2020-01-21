74d0c60ceaca15c74d86c3c3746e62dad0c8004f
Lee <170170@uad.ac.uk>
2020-01-21 13:02:56 +0000

    Added GridScript

--- DES310_Game/Assets/Script/GameLoop.cs
+++ DES310_Game/Assets/Script/GameLoop.cs
@@ -13,7 +13,7 @@ public class GameLoop : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager.GetComponent<GridScript>().CreateGrid();
     }
 
     // Update is called once per frame
---
+++ DES310_Game/Assets/Script/GridScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridScript : MonoBehaviour
+{
+
+
+    public float xStart, yStart;
+
+    public int columnLength, rowLength;
+
+    public int xSpacing;
+    public int ySpacing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   public void CreateGrid()
+    {
+        for (int i = 0; i < columnLength * rowLength; i++)
+        {
+            GameObject.Instantiate(Resources.Load("Cube"), new Vector3(xStart + (xSpacing * (i % columnLength),yStart + (ySpacing * (i / columnLength)), Quaternion.identity);
+        }
+    }
+
+}
