98cfe5025b8d6b9c6a3685844965102178641eb9
Lee <170170@uad.ac.uk>
2020-01-21 14:26:11 +0000

    Grid Script finished
    Creates a grid that you can edit that sapwns a resource (prefab)

--- DES310_Game/Assets/Script/GridScript.cs
+++ DES310_Game/Assets/Script/GridScript.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class GridScript : MonoBehaviour
 {
-
-
     public float xStart, yStart;
 
     public int columnLength, rowLength;
 
-    public int xSpacing;
-    public int ySpacing;
+    public float xSpacing;
+    public float ySpacing;
 
     // Start is called before the first frame update
     void Start()
     {
- 
+        if(xSpacing< 1)
+        {
+            xSpacing = 1;
+        }
+
+        if (ySpacing < 1)
+        {
+            ySpacing = 1;
+        }
     }
 
     // Update is called once per frame
@@ -29,7 +35,7 @@ public class GridScript : MonoBehaviour
     {
         for (int i = 0; i < columnLength * rowLength; i++)
         {
-            GameObject.Instantiate(Resources.Load("Cube"), new Vector3(xStart + (xSpacing * (i % columnLength),yStart + (ySpacing * (i / columnLength)), Quaternion.identity);
+            GameObject.Instantiate(Resources.Load("Cube"), new Vector3((xSpacing * (i % columnLength)), 1.0f, (ySpacing * (i / rowLength))), Quaternion.identity);
         }
     }
 
