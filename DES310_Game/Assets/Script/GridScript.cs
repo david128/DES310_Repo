dd4b91ae2c4fc16e5cb5b22e4b00ca986a36bcf5
Lee <170170@uad.ac.uk>
2020-01-24 19:49:16 +0000

    Grid Selection Complete
    Can fully choose which grid tile the player wants to change.
    
    Added ID checks with old asset and remove/add to the list of objects on the grid.
    
    Just need to change which asset it changes to when selected.
    
    Co-Authored-By: david128 <david128@users.noreply.github.com>

--- DES310_Game/Assets/Script/GridScript.cs
+++ DES310_Game/Assets/Script/GridScript.cs
@@ -19,6 +19,50 @@ public class GridScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RemoveGridTile(GameObject ob)
+    {
+        gridSquares.Remove(ob);
+    }
+
+    public void AddGridTile(GameObject ob)
+    {
+        gridSquares.Add(ob);
+    }
+
+   public GameObject GetGridTile(int id)
+   {
+        bool found = false;
+        int count = 0;
+
+        while(found == false)
+        {
+            if (gridSquares[count].GetComponent<ObjectInfo>().GetObjectID() == id)
+            {
+                
+                found = true;
+            }
+
+            count += 1;
+        }
+
+
+        return gridSquares[count - 1];
+   }
+
+
+   public void CreateGrid()
+    { 
+        int id = 0;
+
         if(xSpacing< 1)
         {
             xSpacing = 1;
@@ -28,17 +72,6 @@ public class GridScript : MonoBehaviour
         {
             zSpacing = 1;
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-   public void CreateGrid()
-    { 
-        int id = 0;
 
         for (int i = 0; i < columnLength; i++)
         {
