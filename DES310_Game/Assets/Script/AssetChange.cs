ba90a8a4c908fdacc12a0290e64e981eb7f6839e
david128 <davidireland128@gmail.com>
2020-02-04 13:50:12 +0000

    Change asset overhaul, options demolish, build, upgrade
    See issue #37

--- DES310_Game/Assets/Script/AssetChange.cs
+++ DES310_Game/Assets/Script/AssetChange.cs
@@ -18,6 +18,76 @@ public class AssetChange : MonoBehaviour
     public GameObject gameManager;
     GameObject newAsset;
 
+
+    public void Upgrade(int id) //upgrade level and then change asset
+    {
+       // int oldID;
+        GameObject asset;
+        asset = gameManager.GetComponent<GridScript>().GetGridTile(id);
+
+        //get level and upgrade
+        int level = asset.GetComponent<ObjectInfo>().GetObjectLevel() + 1;
+
+        //get type
+        int type = asset.GetComponent<ObjectInfo>().GetObjectType();
+
+        NewChangeAsset(id, level, type);
+    }
+
+    public void Demolish(int id) //change to empty
+    {
+
+        NewChangeAsset(id, 0, 0);//change last 0 to empty type
+    }
+
+    public void Build(int id, int type)//build new tile
+    {
+        NewChangeAsset(id, 0, type);
+    }
+
+
+    public GameObject LoadAsset(int type, Transform transform)//load asset based on type
+    {
+
+        switch(type)
+        {
+            case 0:
+                return (GameObject)Instantiate(Resources.Load("Cube"), transform.position, Quaternion.identity);
+                break;
+            default:
+                return (GameObject)Instantiate(Resources.Load("Cube"), transform.position, Quaternion.identity);
+                break;
+
+        }
+          
+    }
+
+
+    public void NewChangeAsset(int id, int level, int type)
+    {
+
+        //find asset and keep transform
+        GameObject asset; 
+        asset = gameManager.GetComponent<GridScript>().GetGridTile(id);
+        Transform transform = asset.transform;
+
+        //load correct asset based on type
+        newAsset = LoadAsset(type, transform);
+
+        //remove from list
+        gameManager.GetComponent<GridScript>().RemoveGridTile(asset);
+
+        //Destroy shape to be replaced
+        GameObject.Destroy(asset);
+
+        //set objectID and level
+        newAsset.GetComponent<ObjectInfo>().SetObjectID(id);
+        newAsset.GetComponent<ObjectInfo>().SetObjectLevel(level);
+
+        //Add from list
+        gameManager.GetComponent<GridScript>().AddGridTile(newAsset);
+    }
+
     public void ChangeAsset(int id)
     {
         //Declare variables
