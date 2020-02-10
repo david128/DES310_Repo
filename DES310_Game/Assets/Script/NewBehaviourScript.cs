7d6ff273dc20ffc9ac437ca9610d091ce0e8174d
david128 <davidireland128@gmail.com>
2020-02-10 18:05:55 +0000

    emptys added

---
+++ DES310_Game/Assets/Script/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public RuntimeAnimatorController animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.AddComponent<Animator>();
+        gameObject.GetComponent<Animator>().runtimeAnimatorController = animator;
+    }
+
+    void Update()
+    {
+        gameObject.GetComponent<Animator>().SetTrigger("trig");
+    }
+
+
+}
--- DES310_Game/Assets/Script/Radial/RadialMenuSpawner.cs
+++ DES310_Game/Assets/Script/Radial/RadialMenuSpawner.cs
@@ -18,6 +18,7 @@ public class RadialMenuSpawner : MonoBehaviour
         RadialMenu newMenu = Instantiate(menuPrefab) as RadialMenu;
         newMenu.transform.SetParent(transform, false);
         newMenu.transform.position = Input.mousePosition;
+        
         newMenu.label.text = obj.title.ToUpper();
         newMenu.SpawnButtons(obj);
         
