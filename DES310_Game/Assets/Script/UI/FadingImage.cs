1eed3fbf09a676240a300225da4f231b3e00c02c
DANIEL MACLEAN <1703020@uad.ac.uk>
2020-03-12 18:24:57 +0000

    UI Update

---
+++ DES310_Game/Assets/Script/UI/FadingImage.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class FadingImage : MonoBehaviour
+{
+    public float FadeTime = 0.01f;
+
+    public IEnumerator FadeImageToFullAlpha(float t)
+    {
+        while (this.GetComponent<Image>().color.a < 1.0f)
+        {
+            this.GetComponent<Image>().color = new Color(this.GetComponent<Image>().color.r, this.GetComponent<Image>().color.g, this.GetComponent<Image>().color.b, this.GetComponent<Image>().color.a + (t));
+            yield return null;
+        }
+    }
+
+    public IEnumerator FadeImageToZeroAlpha(float t)
+    {
+
+        while (this.GetComponent<Image>().color.a > 0.0f)
+        {
+            this.GetComponent<Image>().color = new Color(this.GetComponent<Image>().color.r, this.GetComponent<Image>().color.g, this.GetComponent<Image>().color.b, this.GetComponent<Image>().color.a - (t));
+            yield return null;
+        }
+    }
+
+    void Start()
+    {
+        this.GetComponent<Image>().color = new Color(this.GetComponent<Image>().color.r, this.GetComponent<Image>().color.g, this.GetComponent<Image>().color.b, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(FadeImageToFullAlpha(Time.deltaTime * FadeTime));
+    }
+}
---
+++ DES310_Game/Assets/Script/UI/FadingText.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class FadingText : MonoBehaviour
+{
+    public float FadeTime = 0.01f;
+
+    public IEnumerator FadeTextToFullAlpha(float t)
+    {
+        while (this.GetComponent<Text>().color.a < 1.0f)
+        {
+            this.GetComponent<Text>().color = new Color(this.GetComponent<Text>().color.r, this.GetComponent<Text>().color.g, this.GetComponent<Text>().color.b, this.GetComponent<Text>().color.a + (t));
+            yield return null;
+        }
+    }
+
+    public IEnumerator FadeTextToZeroAlpha(float t)
+    {
+      
+        while (this.GetComponent<Text>().color.a > 0.0f)
+        {
+            this.GetComponent<Text>().color = new Color(this.GetComponent<Text>().color.r, this.GetComponent<Text>().color.g, this.GetComponent<Text>().color.b, this.GetComponent<Text>().color.a - (t));
+            yield return null;
+        }
+    }
+
+    void Start()
+    {
+        this.GetComponent<Text>().color = new Color(this.GetComponent<Text>().color.r, this.GetComponent<Text>().color.g, this.GetComponent<Text>().color.b, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(FadeTextToFullAlpha(Time.deltaTime * FadeTime));
+    }
+}
