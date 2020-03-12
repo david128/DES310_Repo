4e4c787944921daf32f89274e0dd26eebbd809f0
Lee <59842982+leegillan@users.noreply.github.com>
2020-03-12 15:44:48 +0000

    Revert "Deleted Import"

---
+++ DES310_Game/Assets/Wwise/WwiseScript/E_MenuMusic.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class E_MenuMusic : MonoBehaviour
+{
+
+    public void onClick()
+
+    {
+
+        AkSoundEngine.PostEvent("MenuSound", gameObject);
+
+    }
+}
