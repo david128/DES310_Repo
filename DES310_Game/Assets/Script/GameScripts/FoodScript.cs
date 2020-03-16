0e8faf2421544e37536417807fe318395986025a
Lee <170170@uad.ac.uk>
2020-03-16 15:04:55 +0000

    Added new food output bar with the time bar above

--- DES310_Game/Assets/Script/GameScripts/FoodScript.cs
+++ DES310_Game/Assets/Script/GameScripts/FoodScript.cs
@@ -1,17 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FoodScript : MonoBehaviour
 {
+    //Sets manager
+    public GameObject gameManager;
+
     //Declare food variable
-    public int food;
+    public float food;
+    public float maxFood = 10000;
+    public float minFood = 0;
+    public float currentFood;
+    public Image foodBar;
+
+    //Delcare time variables
+    public float maxTime = 10;
+    public float minTime = 0;
+    public float currentTime;
+    public Image timeBar;
+
+    float time;
+
+    //Amount required for quota
+    float quotaAmount = 10000;
 
     //getter and setter
-    public int GetFood() { return food; }
-    public void SetFood(int f) { food = f; }
+    public float GetFood() { return food; }
+    public void SetFood(float f) { food = f; }
 
     //Add to current food
-    public void AddFood(int f) { food = food + f; }
+    public void AddFood(float f) { food = food + f; }
+
+    void Update()
+    {
+        //Updates time variables for the time bar
+        time += Time.deltaTime;
+        currentTime = time;
+        timeBar.fillAmount = currentTime / maxTime;
+
+        //Updates food variables for the food bar
+        currentFood = food;
+        foodBar.fillAmount = currentFood / maxFood;
+
+        if (time >= maxTime)
+        {
+            time = 0;
+            currentFood = 0;
+
+            if (food > quotaAmount)
+            {
+                float foodOver;
+                int moneyGain;
+
+                foodOver = quotaAmount - currentFood;
+
+            }
+
+            food -= food;
+        }
+
+    }
 
 }
