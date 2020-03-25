e0094e6113efd2bf4db23911ff22d2f8cfe560b7
d <d>
2020-03-25 13:33:06 +0000

    Fix for fill not being there with animal fields

--- DES310_Game/Assets/Script/GameScripts/AssetChange.cs
+++ DES310_Game/Assets/Script/GameScripts/AssetChange.cs
@@ -33,7 +33,7 @@ public class AssetChange : MonoBehaviour
         //set fill
         ObjectFill.FillType fill;
 
-        if (type == ObjectInfo.ObjectType.FIELD)
+        if (type == ObjectInfo.ObjectType.FIELD || type == ObjectInfo.ObjectType.CHICKEN_COOP || type == ObjectInfo.ObjectType.COW_FIELD || type == ObjectInfo.ObjectType.PIG_PEN)
         {
             fill = asset.GetComponent<ObjectFill>().GetFillType();
         }
@@ -142,15 +142,12 @@ public class AssetChange : MonoBehaviour
         }
 
         //Checks if the object being changed is a field or not to set the fill of the grid
-        if (type == ObjectInfo.ObjectType.FIELD || type == ObjectInfo.ObjectType.CHICKEN_COOP || type == ObjectInfo.ObjectType.COW_FIELD || type == ObjectInfo.ObjectType.PIG_PEN)
+        if (fill != ObjectFill.FillType.NONE)
         {
             //load correct fill based on whats chosen 
             newFill = LoadFill(fill, transform, level);
         }
-        else
-        {
-            fill = ObjectFill.FillType.NONE;
-        }
+
 
         if (asset.TryGetComponent<Animator>(out anim))
         {
