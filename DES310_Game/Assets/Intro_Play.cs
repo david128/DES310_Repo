e3c0bbf0307a2042ad1f0433db3859dbb5ecd68d
Danielmaclean <61284339+Danielmaclean@users.noreply.github.com>
2020-04-13 19:29:10 +0100

    yada

---
+++ DES310_Game/Assets/Intro_Play.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Intro_Play : MonoBehaviour {
+
+    private string movie = Resources.Load<Video Player("Final_Audio");
+
+    void Start () 
+    {
+        StartCoroutine(streamVideo(movie));
+    }
+
+    private IEnumerator streamVideo(string video)
+    {
+        Handheld.PlayFullScreenMovie(video, Color.black, FullScreenMovieControlMode.Hidden, FullScreenMovieScalingMode.Fill);
+        yield return new WaitForEndOfFrame ();
+        SceneManager.LoadScene (0);
+    }
+}
