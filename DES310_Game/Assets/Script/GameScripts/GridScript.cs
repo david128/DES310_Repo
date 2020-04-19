d7500241f1d1182553ba954402bef133f1bdd327
Lee <1701370@uad.ac.uk>
2020-04-19 04:32:47 +0100

    Added outside asset/Seasonal colour changes
    Game scene now changes colour depending on how long the player has been playing.
    
    Spring
    Summer
    Autumn
    Winter
    
    Added Main Menu button to end screen which completes full game loop. Making the game playable form start to end and back again.
    
    Added outside grid asset to fill the sides of the
    grid and gives the game more population.
    
    Changed shadows given from light to help performance a bit (small but everything counts)
    
    Changed colour of roof of houses as to not blend in with the ground (brown -> black)
    
    Changed materials used, now dont show reflections unless they have to same with; same with specular highlights.
    
    BUG FIX: lockable area didn't fill screen, made it bigger to cover all areas.

--- DES310_Game/Assets/Script/GameScripts/GridScript.cs
+++ DES310_Game/Assets/Script/GameScripts/GridScript.cs
@@ -74,11 +74,12 @@ public class GridScript : MonoBehaviour
         //spawns outside ground
         Instantiate(Resources.Load("Grid"), new Vector3(36.0f, 1.0f, 25.9f), Quaternion.identity);
         Instantiate(Resources.Load("GridFill"), new Vector3(36.0f, 1.0f, 25.9f), Quaternion.identity);
+        Instantiate(Resources.Load("OutsideArea"), new Vector3(36.0f, -0.3f, 25.9f), Quaternion.identity);
 
         Instantiate(Resources.Load("Tractor"), new Vector3(64.0f, 2.0f, 33.0f), new Quaternion(0.0f, 0.225f, 0.0f, 0.974f));
 
         //Spawns locked area blockers
-        lockLvl2 = Instantiate(lockLvl2, new Vector3(76.64f, 12.0f, -14.213f), Quaternion.identity);
+        lockLvl2 = Instantiate(lockLvl2, new Vector3(76.64f, 12.0f, -21.98f), Quaternion.identity);
 
         lockLvl3 = Instantiate(lockLvl3, new Vector3(-11.89f, 12.0f, 87.5f), new Quaternion(0.0f, 0.7071f, 0.0f, 0.7071f));
 
