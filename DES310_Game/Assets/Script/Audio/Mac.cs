fc22ee59f2b7a79708dbfcf6f9c53cc0b8983a99
Rizward <rizwan_walji@hotmail.com>
2020-04-26 18:51:06 +0100

    Updated Sounds, Not Yet Finished
    Added Songs
    Added Sounds for Mac
    Fixed some errors
    Tutorial Mac Sounds missing

---
+++ DES310_Game/Assets/Script/Audio/Mac.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mac : MonoBehaviour
+{
+    public void onClick()
+    {
+        AkSoundEngine.PostEvent("Mac", gameObject);
+    }
+}
