621326e75400ece60dac8cbbcb1be4b04b6f75c2
Lee <1701370@uad.ac.uk>
2020-04-27 16:25:59 +0100

    Added fix for selecting after closing warning boxes
    Some commentary

--- DES310_Game/Assets/Script/UI/Marketplace/MarketplaceSpawner.cs
+++ DES310_Game/Assets/Script/UI/Marketplace/MarketplaceSpawner.cs
@@ -10,6 +10,9 @@ public class MarketplaceSpawner : MonoBehaviour
     {
         GameObject newMenu = Instantiate(menuPrefab, GameObject.FindGameObjectWithTag("Canvas").transform) as GameObject;
 
+        //Gives marketplace menu a while awake script component
+        newMenu.gameObject.AddComponent<WhileAwake>();
+
         newMenu.transform.SetSiblingIndex(6);
     }
 }
---
+++ DES310_Game/Assets/Script/UI/WhileAwake.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhileAwake : MonoBehaviour
+{
+    GameObject gameManager;
+
+    private void Start()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameController");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameManager.GetComponent<InputScript>().GetAllowSelecting() == true && gameObject.activeSelf == true)
+        {
+            gameManager.GetComponent<InputScript>().SetAllowSelecting(false);
+        }
+    }
+}
